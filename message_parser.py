8315da66ebad360e8269527c1438240977ceaddd
sasuke <sasukerx@outlook.jp>
2025-06-10 08:49:46 +0900

    と思ったけどやっぱり大文字と小文字を区別させる

--- message_parser.py
+++ message_parser.py
@@ -44,7 +44,7 @@ def replace_word(text):
     def repl(match):
         key= match.group(0)
         for pattern, value in dictionary.items():
-            if re.fullmatch(pattern, key):
+            if re.fullmatch(pattern, key, flags=re.IGNORECASE):
                 # print(f"{pattern}を{value}に置換")
                 return str(value)
         return key
